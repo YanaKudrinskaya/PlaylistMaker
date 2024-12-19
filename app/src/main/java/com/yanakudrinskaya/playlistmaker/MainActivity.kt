1064ca9970261d3c6ae1810b5464b4941c0eedc5
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2024-12-19 09:08:19 +0300

    Второе задание. Установка заглушек на кнопки главного экрана Toast

--- app/src/main/java/com/yanakudrinskaya/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/yanakudrinskaya/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.yanakudrinskaya.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        searchButton.setOnClickListener(buttonClickListener)
+
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Медиатеку", Toast.LENGTH_SHORT).show()
+        }
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Настройки", Toast.LENGTH_SHORT).show()
         }
     }
 }
